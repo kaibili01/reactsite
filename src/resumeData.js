4649cc21c7d071b80446a43d7e2445488291b3f5
Adan <adananimaldesign@gmail.com>
2020-02-08 11:55:00 -0800

    A new react website in progress

---
+++ src/resumeData.js
@@ -0,0 +1,132 @@
+let resumeData = {
+    "imagebaseurl":"https://adanimaldesign.org/",
+    "name": "Adan",
+    "role": "Frontend Web Developer and Graphic Designer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I love making web pages, beautiful sketches, and exiting visual interfaces.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://linkedin.com/in/adan-garcia-bugarin-18704825",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/kaibili01",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/AdanGBin",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently seeking employment",
+    "address":"Oakland,Ca",
+    "website":"adanimaldesign.org",
+    
+    "education":[
+      {
+        "UniversityName":"UC Berkeley Extention",
+        "specialization":"Full Stack Developer Certification",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2019",
+        "Achievements":"Most Innovative Application Concept Award"
+      },
+      {
+        "UniversityName":"California College of the Arts",
+        "specialization":"MFA Design",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2012",
+        "Achievements":"AIGC Grant recipient: The American Indian Graduate Center"
+      },
+      {
+        "UniversityName":"San Francisco State University",
+        "specialization":"(BA) Industrial Arts and Spanish",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2008",
+        "Achievements":"Dual Degree"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Alameda Health Partners",
+        "specialization":"Graphic Design Contractor",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2017",
+        "Achievements":"Print and Layout Design"
+      },
+      {
+        "CompanyName":"El Camino World Travel",
+        "specialization":"Web Design Contractor",
+        "MonthOfLeaving":"February",
+        "YearOfLeaving":"2016",
+        "Achievements":"CMS Wordpress e-commerce integration"
+      },
+      {
+        "CompanyName":"California College of the Arts",
+        "specialization":"Graphic Designer",
+        "MonthOfLeaving":"February",
+        "YearOfLeaving":"2011",
+        "Achievements":"Master Practice Map and Location design"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"reactjs"
+      },
+      {
+        "skillname":"nodejs"
+      },
+      {
+        "skillname":"Photoshop"
+      },
+      {
+        "skillname":"illustrator"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"https://rocky-bayou-19936.herokuapp.com/"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"../public/images/portfolio/modals/m-coffee.jpg"
+        // "imgurl":"/images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Adan brought detailed strategy and enthusiasm to each design project...",
+        "name":"Jason Engelund",
+        "role":"Artist / Art Teacher / Program Director for Strategic, Sustainable, Social Arts Endeavors" 
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
